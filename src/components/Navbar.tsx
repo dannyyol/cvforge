f429b2ab8b5cc137cffbc2282c64e89dcbcf7fc2
Ayo Omotayo <ayonifeoluwa@gmail.com>
2025-10-01 19:37:26 +0100

    adding navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Navbar: React.FC = () => {
+
+  return (
+    <nav className="h-16 bg-white border-b border-gray-200 flex items-center justify-between px-6">
+      
+    </nav>
+  );
+};
